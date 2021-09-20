14d1f1565a9322749fa6212bcec69af6c72dfa42
MOJAVE <mojave@MBP-di-MOJAVE.home>
2021-09-20 21:50:28 +0200

    add ec2

--- main.tf
+++ main.tf
@@ -1,47 +1,116 @@
 provider "aws" {
-    region = "eu-central-1"
+  region = "eu-central-1"
 }
 
-resource "aws_vpc" "development-vpc" {
-    cidr_block = var.cidr_block[0]
-    tags = {
-      "Name" = "development"
-    }
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "myip" {}
+variable "instance_type" {}
+variable "public_key" {}
+
+
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name: "${var.env_prefix}-vpc" 
+   
+}
 }
 
-variable "cidr_block" {
-    description = "vpc and subnet cidr block"
-    type = list(string)
+resource "aws_subnet" "myapp-subnet-1" {
+ vpc_id = aws_vpc.myapp-vpc.id
+ cidr_block = var.subnet_cidr_block
+ availability_zone = var.avail_zone 
+ tags = {
+   Name: "${var.env_prefix}-subnet-1"
+ }
 }
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_block[1]
-    availability_zone = "eu-central-1a"
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = { Name: "${var.env_prefix}-igw"
+   }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+      tags = {
+      Name: "${var.env_prefix}-main-rtb" 
+  }
+}
+
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.myip]
+  } 
+
+   ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+    egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  } 
         tags = {
-      "Name" = "subnet-1-dev"
+      Name: "${var.env_prefix}-default-sg" 
+  }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+   most_recent = true
+   owners = ["amazon"]
+   filter {
+     name = "name"
+     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+   }
+    filter {
+      name = "virtualization-type"
+      values = ["hvm"]
     }
 }
 
-
-data "aws_vpc" "existing_vpc" {
-    default = true 
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = var.public_key
+  
 }
 
-
-
-resource "aws_subnet" "dev-subnet-2" {
-    vpc_id = data.aws_vpc.existing_vpc.id
-    cidr_block = "172.31.48.0/20"
-    availability_zone = "eu-central-1a"
-         tags = {
-      "Name" = "default-subnet-2"
-    }
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id] 
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+  user_data = file("entry-script.sh")
 }
 
-output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
 }
 
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
+output "ec2_public_ip" {
+value = aws_instance.myapp-server.public_ip
 }
